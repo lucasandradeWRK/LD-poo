dcd1097274feded0c89e053a1de037cbe686fc50
Lucas Andrade Rodrigues <lucas.andrade.rodrigues.13@gmail.com>
2017-11-29 23:37:16 -0200

    Update usuario.java

--- usuario.java
+++ usuario.java
@@ -2,11 +2,6 @@
 *Grupo: Lucas Andrade Rodrigues, Luan dos Santos Cantalice
 *
 */
-import javax.swing.JOptionPane;
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
 public class usuario{
 	String nome;
